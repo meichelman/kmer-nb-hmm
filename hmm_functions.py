b15ee96b76a52bebdb2c148562577fc10eb7ed2d
Matthew Eichelman <meichelman5@gmail.com>
2026-04-07 13:59:31 -0500

    Testing

--- hmm_functions.py
+++ hmm_functions.py
@@ -92,7 +92,7 @@ def NB_probability_underflow_safe(k, mu, r):
     # NOTE: we return log_nb instead of the actual probability to avoid underflow, the exact value is less important than the relative values when comparing states
 
 
-@njit
+# @njit
 def Emission_probs(emissions, observations, dispersions,mutrates, window_size):
     n = len(observations)
     n_states = len(emissions)          
