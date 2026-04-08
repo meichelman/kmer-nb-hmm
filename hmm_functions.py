3976cabf65de20925189f757888021fccfa9d758
Matthew Eichelman <meichelman5@gmail.com>
2026-04-08 16:52:53 -0500

    Improved decoding reporting

--- hmm_functions.py
+++ hmm_functions.py
@@ -330,19 +330,22 @@ def Viterbi_path(emissions, hmm_parameters):
 
     return viterbi_path
 
+
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Write segments to output file
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def Write_posterior_probs(obs, mutrates, post_seq, post_path, viterbi_path, hmm_parameters, filename, window_size):
+def Write_posterior_probs(obs, obs_rates, post_seq, post_path, viterbi_path, hmm_parameters, filename, window_size):
     post_seq = post_seq.T
 
     with open(filename, 'w') as out:
-        state_names = '\t'.join(hmm_parameters.state_names)
-        out.write('start\tend\tnum_kmers\tmutationrate\t' + state_names + '\tposterior_state\tviterbi_state\n')
+        state_names = ''
+        for state in hmm_parameters.state_names:
+            state_names += f'p{state}\t'
+        out.write(f'start\tend\tcount\tobs_rate\t{state_names}\tposterior_state\tviterbi_state\n')
 
         i = 0
-        for (obs, m, posterior, post_state, viterbi_state) in zip(obs, mutrates, post_seq, post_path, viterbi_path):
+        for (obs, obs_rate, posterior, post_state, viterbi_state) in zip(obs, obs_rates, post_seq, post_path, viterbi_path):
             posterior_to_print = '\t'.join([str(round(x, 4)) for x in posterior])
-            out.write(f'{i * window_size}\t{(i + 1) * window_size}\t{obs}\t{m}\t{posterior_to_print}\t{hmm_parameters.state_names[post_state]}\t{hmm_parameters.state_names[viterbi_state]}\n')
+            out.write(f'{i * window_size}\t{(i + 1) * window_size}\t{obs}\t{obs_rate}\t{posterior_to_print}\t{hmm_parameters.state_names[post_state]}\t\t{hmm_parameters.state_names[viterbi_state]}\n')
             i += 1
